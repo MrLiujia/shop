eb058f1d947f70adefd754a294742602c40afbc0
Mr Liu <1056033459@qq.com>
2018-07-16 20:16:48 +0800

    配置MyBatis

--- src/main/java/shop/config/AppConfig.java
+++ src/main/java/shop/config/AppConfig.java
@@ -1,18 +1,46 @@
 package shop.config;
 
+import javax.sql.DataSource;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-
 @Configuration
 @ComponentScan("shop")
 @EnableWebMvc // 开启web mvc基础设施支持
+@MapperScan("shop.mapper")
+@PropertySource("classpath:jdbc.properties")
 public class AppConfig extends WebMvcConfigurerAdapter{
 	public void configureViewResolvers(ViewResolverRegistry rigistry) {
 		rigistry.jsp("/WEB-INF/jsp",".jsp");
+	}
+	
+	@Bean
+	public DataSource dataSource(Environment env) { 
+		DriverManagerDataSource ds = new DriverManagerDataSource(
+				env.getProperty("jdbc.url"),
+				env.getProperty("jdbc.username"),
+				env.getProperty("jdbc.password"));
+		ds.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		return ds;
+	}	
+	
+	@Bean // 定义Mybatis的会话工厂
+	public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) {
+		SqlSessionFactoryBean sf = new SqlSessionFactoryBean();
+		sf.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
+		sf.setDataSource(dataSource);
+		return sf;
 	}
 		
 }
